cf13ff89f069e3dace6cb83a970475d846787638
Finley <finley.mwachia12@gmail.com>
2024-06-05 17:58:49 +0300

    make changes

--- index.js
+++ index.js
@@ -3,6 +3,6 @@ const app = express();
 const port = 3000;
 
 app.listen(port,()=>{
-    console.log(`Listening on port ${port}`);
+    console.log(`This server is Listening on port ${port}`);
 });
 
